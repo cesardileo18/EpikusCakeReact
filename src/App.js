068c261633039baad047cda65e8e0f6bd6030a35
cesar.dileonardo <cesar.dileonardo@dataiq.com.ar>
2023-03-08 15:57:50 -0300

    cambios...

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ function App() {
       <BrowserRouter basename='/EpikusCakeReact'>
         <Routes>
           <Route path='/EpikusCakeReact' element={<Inicio />} />
-          <Route path='/Inicio' element={<Inicio />} />
-          <Route path='/Nosotros' element={<Nosotros />} />
-          <Route path='/Pasteleria' element={<Pasteleria />} />
-          <Route path='/Tortas' element={<Tortas />} />
-          <Route path='/Contacto' element={<Contacto />} />
+          <Route path='/EpikusCakeReact/Inicio' element={<Inicio />} />
+          <Route path='/EpikusCakeReact/Nosotros' element={<Nosotros />} />
+          <Route path='/EpikusCakeReact/Pasteleria' element={<Pasteleria />} />
+          <Route path='/EpikusCakeReact/Tortas' element={<Tortas />} />
+          <Route path='/EpikusCakeReact/Contacto' element={<Contacto />} />
         </Routes>
       </BrowserRouter>
     </div>
