2455fee85eceb9cce3945b3c46e2ed4d928eddc1
cesar.dileonardo <cesar.dileonardo@dataiq.com.ar>
2023-03-08 15:28:06 -0300

    Prueba con basename en HasRouter

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
         <App />
     </BrowserRouter> */
 root.render(
-    <HashRouter>
+    <HashRouter basename='/EpikusCakeReact'>
         <App />
     </HashRouter>
 );
